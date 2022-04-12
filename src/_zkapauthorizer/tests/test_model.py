ee88048e87978d26da5f0e89ffcddb3744150148
meejah <meejah@meejah.ca>
2022-04-11 23:05:23 -0600

    format

--- src/_zkapauthorizer/tests/test_model.py
+++ src/_zkapauthorizer/tests/test_model.py
@@ -972,7 +972,7 @@ class EventStreamTests(TestCase):
                 lambda x: x[0](x[1], x[2]),
             ),
             min_size=2,
-        )
+        ),
     )
     def test_event_stream_prune(self, get_config, changes):
         """
@@ -997,8 +997,7 @@ class EventStreamTests(TestCase):
         post_events = store.get_events()
 
         self.assertThat(
-            len(post_events.changes) + where,
-            Equals(len(pre_events.changes))
+            len(post_events.changes) + where, Equals(len(pre_events.changes))
         )
 
 
